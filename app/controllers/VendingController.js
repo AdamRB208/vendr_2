f5255118a482a193c736ab864f6e6adb50f051fa
AdamRB208 <adamrborup@gmail.com>
2025-04-01 11:46:50 -0600

    add money button works and styled buy buttons.

--- app/controllers/VendingController.js
+++ app/controllers/VendingController.js
@@ -1,6 +1,18 @@
+import { AppState } from "../AppState.js"
+import { moneyService } from "../services/MoneyService.js"
 
 export class VendingController {
   constructor() {
     console.log('Vending Controller Loaded!')
+    AppState.on('money', this.drawMoney)
+  }
+
+  addQuarter() {
+    moneyService.addQuarter()
+  }
+
+  drawMoney() {
+    const moneyAsCurrency = new Intl.NumberFormat('en-us', { style: 'currency', currency: 'USD' }).format(AppState.money)
+    // setText('total-money', moneyAsCurrency)
   }
 }
