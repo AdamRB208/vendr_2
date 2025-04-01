61949b3daa41ce24663ed5c1bada4952a81ec31a
AdamRB208 <adamrborup@gmail.com>
2025-04-01 09:44:29 -0600

    created snack model with card template

---
+++ app/models/Snacks.js
@@ -0,0 +1,23 @@
+
+
+
+export class Snacks {
+  constructor(data) {
+    this.name = data.name
+    this.price = data.price
+    this.imgUrl = data.imgUrl
+  }
+
+
+  get cardTemplate() {
+    return /*html*/ `
+    <div class="col 4">
+      <div></div>
+        <img class="item-img" src=""
+      alt="image of vending machine items">
+        <span>item name</span>
+        <div><button class="mt-2 mb-3 p-1">$.25</button></div>
+      </div>
+    `
+  }
+}
